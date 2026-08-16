1ccc250cbdf9ba198984e81013069946f07e95c6
Pichau <gabrielribeneves@gmail.com>
2026-08-16 17:05:09 -0300

    codigo para sorteio

---
+++ codigo para sorteio/main.py
@@ -0,0 +1,14 @@
+import random
+
+
+convidados = ["Ana", "Lucas", "João", "Marina", "Pedro", "Carla", "Ricardo", "Fernanda"]
+
+premios = ["Bicicleta", "Tablet", "Fone de ouvido", "Livro", "Camisa"]
+
+contador = 0 
+
+while contador < 5:
+    premio = random.sample(premios, k=1)
+    convidado = random.sample(convidados, k=1)
+    contador += 1
+    print(f'{convidado} recebeu {premio}')
